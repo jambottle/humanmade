203cec002612667034653f16b002972bac0937a6
jambottle <jaewon.walks@gmail.com>
2024-12-12 22:55:08 +0900

    chore: ESLint 설정 추가
    - eslint-plugin-simple-import-sort: import 구문 자동 정렬
    - eslint-plugin-unused-imports: import 구문 자동 제거
    - 참고: https://univdev.page/posts/nextjs-eslint/

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.plugins("simple-import-sort", "unused-imports"),
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+      "unused-imports/no-unused-imports": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
